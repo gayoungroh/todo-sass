127ad443096844d06286239a08dc52bdbcef2d8e
노가영 <8457397@naver.com>
2022-09-21 11:54:06 +0900

    최초업로드

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.scss';
+import TodoContainer from './components/TodoContainer';
+
+const App = ()=> {
+ return (
+<TodoContainer />
+ );
+};
+
+export default App;
---
+++ src/components/TodoHead.jsx
@@ -0,0 +1,11 @@
+import TodoInsert from "./TodoInsert";
+
+const TodoHead = (props)=>{
+  return (
+    <header>
+      <h1>일정관리</h1>
+      <TodoInsert />
+    </header>
+  );
+};
+export default TodoHead;
